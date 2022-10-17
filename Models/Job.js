d8c85389c208c1c6ce7b94da6c545aea8db1c319
saymanrabbi <mdrabbi.asm@gmail.com>
2022-10-17 18:39:58 +0600

    jobs schema

---
+++ Models/Job.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+const JobSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: [true, 'Please add a title'],
+        trim: true,
+        maxlength: [40, 'Title can not be more than 40 characters'],
+        minlength: [3, 'Title can not be less than 3 characters']
+    },
+    description: {
+        type: String,
+        required: [true, 'Please add a description'],
+        maxlength: [500, 'Description can not be more than 500 characters'],
+        minlength: [10, 'Description can not be less than 10 characters']
+    },
+    email: {
+        type: String,
+        required: [true, 'Please add an email'],
+        unique: true,
+        trim: true,
+        lowercase: true,
+        validate: [validator.isEmail, 'Please add a valid email']
+    },
+    phone: {
+        type: String,
+        required: [true, 'Please add a phone number'],
+        validate: [validator.isMobilePhone, 'Please add a valid phone number']
+    },
+    address: {
+        type: String,
+        required: [true, 'Please add an address'],
+        maxlength: [100, 'Address can not be more than 100 characters'],
+        minlength: [10, 'Address can not be less than 10 characters']
+    },
+    postition:{
+        type: String,
+        required: [true, 'Please add a position'],
+        enum:{
+            values:['Frontend Developer', 'Backend Developer', 'Fullstack Developer'],
+            message: 'Please select a position {VALUE}'
+        }
+    },
+    sallery:{
+        type: Number,
+        required: [true, 'Please add a sallery'],
+        min: [1000, 'Sallery can not be less than $1000'],
+    },
+},{
+    timestamps: true
+})
