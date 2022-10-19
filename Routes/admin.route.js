be02485fc9c65b0ae1a0bdd9eec8d4151e8572c0
saymanrabbi <mdrabbi.asm@gmail.com>
2022-10-19 21:37:19 +0600

    use js docs

--- Routes/admin.route.js
+++ Routes/admin.route.js
@@ -4,7 +4,29 @@ const { verifyAdmin } = require('../Middleware/admin');
 const verifyToken = require('../Middleware/verifyToken');
 const router = express.Router();
 router.patch('/user/:id',verifyToken,verifyAdmin,getuserwithId)
+/**
+ * Admin Route only for admin
+ * Api Example: http://localhost:5000/api/v1/admin/user/634fad28987f1381153bb283  
+ * @param {string} id - user id
+ * @returns {object} 200 - An array of user info and update role
+ */
 router.get('/allcandidate',verifyToken,verifyAdmin,getAllCandidate)
+/**
+ * Admin Route only for admin
+ * Api Example: http://localhost:5000/api/v1/admin/candidate/634fad28987f1381153bb283  
+ * @param {string} id - user id
+ */
 router.get('/candidate/:id',verifyToken,verifyAdmin,candidateWithId)
+/**
+ * Admin Route only for admin
+ * Api Example: http://localhost:5000/api/v1/admin/candidate/634fad28987f1381153bb283  
+ * @param {string} id - user id
+ */
 router.get('/hiringmanagers',verifyToken,verifyAdmin,gethiringmanagers)
+/**
+ * Admin Route only for admin
+ * Api Example: http://localhost:5000/api/v1/admin/hiringmanagers  
+ * @param {string} id - user id
+ * @returns {object} 200 - An array of  info
+ */
 module.exports = router
--- Routes/user.route.js
+++ Routes/user.route.js
@@ -4,7 +4,30 @@ const verifyToken = require('../Middleware/verifyToken');
 const router= express.Router();
 // Importing the controller
 router.post('/register',CreateUserContoler)
+/**
+ * user Route
+ * Api Example: http://localhost:5000/api/v1/user/register  
+ * use body to send data
+ * @param {string} name - user name
+ * @param {string} email - user email
+ * @param {string} password - user password
+ * @param {string} role - user role
+ */
 router.post('/login',loginUserControler)
+/**
+ * user Route
+ * Api Example: http://localhost:5000/api/v1/user/login
+ * use body to send data
+ * @param {string} email - user email
+ * @param {string} password - user password
+ * @returns {object} 200 - An array of user info and token
+*/
 router.get('/me',verifyToken,getUserInfo)
-
+/**
+ * user Route
+ * Api Example: http://localhost:5000/api/v1/user/me
+ * use toke to get data
+ * @headers {string} token - user token
+ * @returns {object} 200 - An array of user info
+*/
 module.exports = router;
